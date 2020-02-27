d6fa8a936bc1d2be4620819033ca571d64c6c236
Jose Alves Durand Neto <durand.neto@gmail.com>
2020-02-27 12:02:27 -0800

    compiling index jsx

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-import AvatarSkeleton from './dist/Components/Skeleton/Avatar';
-import TextSkeleton from './dist/Components/Skeleton/Text';
-import DescriptionSkeleton from './dist/Components/Skeleton/Description';
+"use strict";
 
-export { AvatarSkeleton, TextSkeleton, DescriptionSkeleton }
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AvatarSkeleton", {
+  enumerable: true,
+  get: function get() {
+    return _Avatar.default;
+  }
+});
+Object.defineProperty(exports, "TextSkeleton", {
+  enumerable: true,
+  get: function get() {
+    return _Text.default;
+  }
+});
+Object.defineProperty(exports, "DescriptionSkeleton", {
+  enumerable: true,
+  get: function get() {
+    return _Description.default;
+  }
+});
+
+var _Avatar = _interopRequireDefault(require("./dist/Components/Skeleton/Avatar"));
+
+var _Text = _interopRequireDefault(require("./dist/Components/Skeleton/Text"));
+
+var _Description = _interopRequireDefault(require("./dist/Components/Skeleton/Description"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
